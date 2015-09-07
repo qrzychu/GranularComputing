ebaea339a8126f0a23c5e0cb76b5f815f3859b27
Krzysztof Skowronek <qrzychu@gmail.com>
2015-09-08 00:12:22 +0200

    Initial commit

---
+++ fcm.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class FCM(object):
+    def __init__(self, m=2):
+        self.m = m
+
+    def clusterize(self, X, c):
+        """Runs clusterization of X into c fuzzy clusters
+
+        returns ( c - clusters centres, U - membership matrix)
+        """
+
+        U = np.random()
+        pass
